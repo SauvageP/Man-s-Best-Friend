e18bc4f9acb8bbf543ada0db776960a3f1f70707
yung305 <prgabriel88@gmail.com>
2014-10-20 12:26:01 -0400

    Added Dog touch class

---
+++ Man's Best Friend/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  Man's Best Friend
+//
+//  Created by Perry R Gabriel on 10/20/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ Man's Best Friend/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  Man's Best Friend
+//
+//  Created by Perry R Gabriel on 10/20/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
