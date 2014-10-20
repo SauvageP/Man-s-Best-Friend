ddea1d5c70e73a5fb0dd1a605e9b12b37da85d47
yung305 <prgabriel88@gmail.com>
2014-10-20 12:06:12 -0400

    Initial Commit

---
+++ Man's Best Friend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Perry R Gabriel on 10/20/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
